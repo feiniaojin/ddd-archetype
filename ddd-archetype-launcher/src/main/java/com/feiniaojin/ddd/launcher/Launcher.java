a87a92a2b2f9347b744307f5b2193f8236bcfc07
qinyujie <943868899@qq.com>
2023-09-19 11:13:36 +0800

    对脚手架进行了完善： - 加入.gitkeep文件进行占位，避免重新创建 - 把常用的jar包加入进来并调试通过

---
+++ ddd-archetype-launcher/src/main/java/com/feiniaojin/ddd/launcher/Launcher.java
@@ -0,0 +1,6 @@
+package com.feiniaojin.ddd.launcher;
+public class Launcher {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ ddd-archetype-ui-web/src/main/java/com/feiniaojin/ddd/ui/web/WebConfig.java
@@ -0,0 +1,11 @@
+package com.feiniaojin.ddd.ui.web;
+
+import com.feiniaojin.gracefulresponse.EnableGracefulResponse;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableGracefulResponse
+public class WebConfig {
+
+
+}
